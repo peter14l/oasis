d814016376ad8b15c73f65d2023b06fea48f3792
Peter <peter_parker_2008@outlook.com>
2026-04-03 19:02:34 +0530

    Move utils, config, exceptions to core/ and delete old directories
    Moves lib/utils/* -> lib/core/utils/, lib/config/* -> lib/core/config/, lib/exceptions/* -> lib/core/errors/. Deletes old directories after migration.
    
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- lib/exceptions/auth_exception.dart
+++
@@ -1,9 +0,0 @@
-class AuthException implements Exception {
-  final String message;
-  final String? code;
-
-  AuthException(this.message, {this.code});
-
-  @override
-  String toString() => message;
-}
