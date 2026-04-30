fdd7a97cc4f7ad203e525a4d47efa979dc0fa1db
Peter <peter_parker_2008@outlook.com>
2026-04-30 12:49:48 +0530

    fix: remove legacy Firebase Messaging background registration from MainApplication.kt
    - Remove deprecated PluginRegistrantCallback and GeneratedPluginRegistrant
    
    - Resolves Android compilation errors with modern flutter_local_notifications and firebase_messaging V2 embedding

--- android/app/src/main/kotlin/com/oasis/app/MainApplication.kt
+++ android/app/src/main/kotlin/com/oasis/app/MainApplication.kt
@@ -1,22 +1,6 @@
 package com.oasis.app
 
 import io.flutter.app.FlutterApplication
-import io.flutter.plugin.common.PluginRegistry
-import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback
-import io.flutter.plugins.GeneratedPluginRegistrant
-import io.flutter.plugins.firebase.messaging.FlutterFirebaseMessagingBackgroundService
 
-class MainApplication : FlutterApplication(), PluginRegistrantCallback {
-    override fun onCreate() {
-        super.onCreate()
-        // This ensures the background isolate is correctly registered for FCM
-        FlutterFirebaseMessagingBackgroundService.setPluginRegistrant(this)
-    }
-
-    override fun registerWith(registry: PluginRegistry?) {
-        // Register all plugins with the background isolate
-        if (registry != null) {
-            GeneratedPluginRegistrant.registerWith(registry)
-        }
-    }
+class MainApplication : FlutterApplication() {
 }
